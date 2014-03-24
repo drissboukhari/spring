2f908509989b2b671d409d37ec040c4e7e5b156a
elmehdibahhar <elmehdibahhar@gmail.com>
2014-03-24 01:03:50 +0000

    spring exemple

---
+++ spring1/src/main/java/spring/exemple/spring1/App.java
@@ -0,0 +1,15 @@
+package spring.exemple.spring1;
+
+import spring.exemple.Lesson1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Lesson1.main(args);
+    }
+}
